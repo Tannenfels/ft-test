b865f9e09883fc143b2df11fba84f54b9c815b98
Tannenfels <tannenfels@users.noreply.github.com>
2026-04-22 06:47:00 +0300

    Done

--- database/migrations/2026_04_15_142607_create_delivery_units_table.php
+++ database/migrations/2026_04_15_142607_create_delivery_units_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('delivery_types', function (Blueprint $table) {
+        Schema::create('delivery_units', function (Blueprint $table) {
             $table->id();
             $table->string('address');
             $table->unsignedBigInteger('type_id');
